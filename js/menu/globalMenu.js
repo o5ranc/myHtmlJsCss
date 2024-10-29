1d001a0dfba134abaf8647c4979515538eee116f
o5ranc <o5ranc@github.com>
2024-10-29 16:51:06 +0900

    메뉴 구성중 1차 반영

---
+++ js/menu/globalMenu.js
@@ -0,0 +1,13 @@
+const globalMenu = [
+  {
+    heading: "Home",
+    route: "/",
+    pages: [
+      {
+        sectionTitle: "exam-components",
+        route: "",
+        fontIcon: "fa fa-home", // svgIcon 형태로도 사용 가능, 정하기 따라
+      },
+    ],
+  },
+];
